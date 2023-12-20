29af71660b25860fe82507837ca34a2ea80b4118
tiago <97364045+tivgo@users.noreply.github.com>
2023-12-20 15:36:12 -0300

    Create 379A - New Year Candles.cpp

---
+++ codeforces/379A - New Year Candles.cpp
@@ -0,0 +1,36 @@
+// 237555892	Dec/16/2023 20:04UTC-3	tiagowhk	379A - New Year Candles	GNU C++20 (64)	Accepted	0 ms	0 KB
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+typedef long long ll;
+typedef long double ld;
+const int N = 2e5+5;
+const ll inf = 1e9;
+
+void solve() {
+  int a, b, d, m;
+  cin >> a >> b;
+  
+  int h = a;
+  
+  while (a >= b) {
+      d = a / b;
+      h += d;
+      m = a % b;
+      a = d + m;
+  }
+  cout << h << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int t = 1;
+  // cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
