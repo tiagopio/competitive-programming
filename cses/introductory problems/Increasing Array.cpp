d1219b315763a7adbd2550c939772a43085ad418
tiago <97364045+tivgo@users.noreply.github.com>
2024-02-23 11:04:53 -0300

    Create Increasing Array.cpp

---
+++ cses/introductory problems/Increasing Array.cpp
@@ -0,0 +1,49 @@
+/*
+  Task:	Increasing Array
+  Sender:	tiagopio
+  Submission time:	2024-02-23 15:45:45 +0200
+  Language:	C++20
+  Status:	READY
+  Result:	ACCEPTED
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+typedef long double ld;
+const int N = 2e5+10;
+const int mod = 1e9+7;
+const ll inf = 1e9+10;
+ 
+void solve() {
+  int n;
+  cin >> n;
+  ll ans = 0, prev;
+  cin >> prev;
+ 
+  for (int i = 1; i < n; i++) {
+    ll e; cin >> e;
+    if (e < prev) {
+      ans += prev - e;
+      prev = e + (prev - e);
+    } else prev = e;
+  }
+  cout << ans << endl;
+}
+ 
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int t = 1;
+  // cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
