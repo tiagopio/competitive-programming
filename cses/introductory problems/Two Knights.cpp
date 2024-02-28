74039cc1c9eb07ead0ce0a3f342e65c2dbb01545
tiago <97364045+tiagopio@users.noreply.github.com>
2024-02-28 16:59:34 -0300

    Create Two Knights.cpp

---
+++ cses/introductory problems/Two Knights.cpp
@@ -0,0 +1,48 @@
+/*
+  Task:	Two Knights
+  Sender:	tiagopio
+  Submission time:	2024-02-28 21:57:49 +0200
+  Language:	C++20
+  Status:	READY
+  Result:	ACCEPTED
+  
+  Test results
+  test	verdict	time	
+  #1	ACCEPTED	0.02 s
+  #2	ACCEPTED	0.00 s
+  #3	ACCEPTED	0.00 s
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+typedef long double ld;
+const int N = 2e5+10;
+const int mod = 1e9+7;
+const ll inf = 1e9+10;
+ 
+void solve() {
+  ll n;
+  cin >> n;
+  
+  cout << 0 << endl;
+  for (ll i = 2; i <= n; i++) {
+    cout << (i * i) * (i * i - 1) / 2 - (4 * (i - 2) * (i - 1)) << endl;
+  }
+}
+ 
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int t = 1;
+  // cin >> t;
+  while(t--) {
+    solve();
+  }
+  return 0;
+}
