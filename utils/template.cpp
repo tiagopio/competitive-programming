505caa98d366f753a402f0fc2b2cdbb0f153170c
tiago <97364045+tiagopio@users.noreply.github.com>
2024-09-08 09:59:58 -0300

    Update template.cpp

--- utils/template.cpp
+++ utils/template.cpp
@@ -1,20 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define dbg(x) cout << #x << " = " << x << endl
-typedef long long ll;
-typedef long double ld;
-const int N = 2e5+10;
-const int mod = 1e9+7;
-const ll inf = 1e9+10;
+#define int long long
 
 void solve() {
   // solve it here
 }
 
-int main() {
+int32_t main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
