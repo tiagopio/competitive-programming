1e080799759a477409a9f895eb5c180ce6ac19d0
tiago <97364045+tiagopio@users.noreply.github.com>
2025-05-08 10:54:43 -0300

    Update Towers.cpp

--- cses/sorting and searching/Towers.cpp
+++ cses/sorting and searching/Towers.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 void solve() {
     int n;
     cin >> n;
@@ -22,14 +22,15 @@ void solve() {
     }
     cout << towers.size() << '\n';
 }
- 
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t = 1;
     // cin >> t;
     while(t--) {
         solve();
     }
     return 0;
+}
