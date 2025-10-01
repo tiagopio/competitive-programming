52805720411cb6027d13f242125d616b83157be2
Tiago Pio <97364045+tiagopio@users.noreply.github.com>
2025-10-01 17:58:05 -0300

    new problem

---
+++ usaco/bronze/complete_search/Diamond Collector.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+void solve() {
+    setIO("diamond");
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int s = a[i] + k;
+        int tmp = 0;
+        for (int j = 0; j < n; j++) {
+            if (i != j and a[j] <= s and a[j] >= a[i]) tmp++; 
+        }
+        ans = max(ans, tmp);
+    }
+    cout << ans + 1 << '\n';
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
