9f258f440982d3faf8e74234046f81ed15b18a50
tiagopio <tiagofonsecapio2018@gmail.com>
2026-05-21 10:45:01 -0300

    abc048a

---
+++ atcoder/abc048/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+    string a, s, c;
+    cin >> a >> s >> c;
+    cout << 'A' << char(toupper(s[0])) << 'C' << '\n';
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    } 
+    return 0;
+}
